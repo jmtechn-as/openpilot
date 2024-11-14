34f46371bb796f14fa258befcbaee86f4cf6c3d2
crwusiz <crwusiz@gmail.com>
2024-11-15 08:20:20 +0900

    fix

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -62,11 +62,10 @@ def manager_init() -> None:
     ("DisableOpFcw", "0"),
     ("ShowDebugUI", "0"),
     ("NewRadarInterface", "0"),
-
-
     ("CommaPedal", "0"),
     ("RestartForceAccel", "0"),
     ("KeepLatWhenBrake","0"),
+    ("SshEnabled", "1"),
   ]
   if not PC:
     default_params.append(("LastUpdateTime", datetime.datetime.utcnow().isoformat().encode('utf8')))
@@ -141,11 +140,6 @@ def manager_cleanup() -> None:
 
 
 def manager_thread() -> None:
-
-  if EON:
-    Process(name="autoshutdownd", target=launcher, args=("selfdrive.autoshutdownd", "autoshutdownd")).start()
-    system("am startservice com.neokii.optool/.MainService")
-
   Process(name="road_speed_limiter", target=launcher, args=("selfdrive.road_speed_limiter", "road_speed_limiter")).start()
   cloudlog.bind(daemon="manager")
   cloudlog.info("manager start")
