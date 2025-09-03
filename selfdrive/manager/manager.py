755c3dfde7748f795c264ae07ed7a6df797893a7
crwusiz <crwusiz@gmail.com>
2025-09-03 14:26:05 +0900

    fix

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -238,7 +238,6 @@ def manager_thread() -> None:
 
   if EON:
     Process(name="autoshutdownd", target=launcher, args=("selfdrive.autoshutdownd", "autoshutdownd")).start()
-    system("am startservice com.neokii.optool/.MainService")
 
   Process(name="road_speed_limiter", target=launcher, args=("selfdrive.road_speed_limiter", "road_speed_limiter")).start()
   cloudlog.bind(daemon="manager")
